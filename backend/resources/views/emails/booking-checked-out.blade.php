4681c1550b1f7eafecd52e1cdff5b9c1f8c46d8f
ducanh186 <leducanh180604@gmail.com>
2026-03-24 14:40:43 +0700

    chore: update backend/resources/views/emails/booking-checked-out.blade.php

---
+++ backend/resources/views/emails/booking-checked-out.blade.php
@@ -0,0 +1,11 @@
+Xin chào {{ $recipientName }},
+
+Cảm ơn bạn đã ở tại Duly's House!
+
+Đơn {{ $booking->booking_code }} đã check-out thành công.
+Tổng tiền thanh toán: {{ number_format($booking->total_amount, 0, ',', '.') }}₫
+
+Chúng tôi rất mong được đón tiếp bạn lần sau. Nếu hài lòng, hãy để lại đánh giá cho chúng tôi nhé!
+
+Trân trọng,
+Duly's House
