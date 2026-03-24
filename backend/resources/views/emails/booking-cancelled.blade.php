b00d1e1b11d7c02fc87a0e84ed172ad5b5d02250
ducanh186 <leducanh180604@gmail.com>
2026-03-24 14:40:42 +0700

    chore: update backend/resources/views/emails/booking-cancelled.blade.php

---
+++ backend/resources/views/emails/booking-cancelled.blade.php
@@ -0,0 +1,8 @@
+Xin chào {{ $recipientName }},
+
+Đơn đặt phòng {{ $booking->booking_code }} đã bị huỷ.
+
+Nếu bạn muốn đặt lại, vui lòng truy cập website hoặc liên hệ trực tiếp với chúng tôi.
+
+Trân trọng,
+Duly's House
