c1ac36d7d994d23f50e89b3737dc81690df0b524
ducanh186 <leducanh180604@gmail.com>
2026-03-29 16:10:23 +0700

    chore: add SoftDeletes to Room model

--- backend/app/Models/Room.php
+++ backend/app/Models/Room.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Room extends Model
 {
-    use HasUuids;
+    use HasUuids, SoftDeletes;
 
     protected $fillable = [
         'room_type_id', 'room_code', 'status', 'cleanliness', 'notes', 'main_image',
